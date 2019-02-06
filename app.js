9362ee854b0980430b63c37d1107158626398fd7
thair alokaily <thairalokaily@gmail.com>
2019-02-05 23:35:50 -0800

    connecting pages using ejs engine

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ app.get("/", (request, res) => {
     res.render("landing");
 });
 
-app.get("/talk of the town", (req, res) => {
+app.get("/talkOfTheTown", (req, res) => {
     console.log("rumors");
+
+    let rumors = [
+        {id: "4231423", class: "VA", level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+        {id: "4332223", class: "DC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+        {id: "4231656", class: "CA" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+        {id: "4231677", class: "NC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+        {id: "4231454", class: "TX" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+        {id: "4231232", class: "FL" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" }
+    ];
+
+    res.render("tott", {rum: rumors});
 });
 
 
