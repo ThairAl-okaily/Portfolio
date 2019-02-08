c89ebf3f78212aa2ca963883c93aed4f8b349919
thair alokaily <thairalokaily@gmail.com>
2019-02-07 22:57:36 -0800

    configuring RESTFull

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+let rumors = [
+    {id: "4231423", class: "VA", level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+    {id: "4332223", class: "DC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+    {id: "4231656", class: "CA" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+    {id: "4231677", class: "NC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+    {id: "4231454", class: "TX" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
+    {id: "4231232", class: "FL" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" }
+];
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (request, res) => {
@@ -10,19 +21,29 @@ app.get("/", (request, res) => {
 app.get("/talkOfTheTown", (req, res) => {
     console.log("rumors");
 
-    let rumors = [
-        {id: "4231423", class: "VA", level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
-        {id: "4332223", class: "DC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
-        {id: "4231656", class: "CA" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
-        {id: "4231677", class: "NC" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
-        {id: "4231454", class: "TX" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" },
-        {id: "4231232", class: "FL" , level: "0", maxLevel: "0", rumor: "sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio" }
-    ];
+
 
     res.render("tott", {rum: rumors});
 });
 
 
+app.post("/talkOfTheTown", (req, res) => {
+    // get data from form and add to rumors array
+    let name = req.body.name;
+    let bodyOfRumor = req.body.body;
+    let newRumor = {class: name, rumor: bodyOfRumor};
+    rumors.push(newRumor);
+
+    //redirect back to talk of the town page
+    res.redirect("/talkOfTheTown");
+});
+
+
+app.get("/talkOfTheTown/new", (req, res) => {
+    res.render("new.ejs");
+
+
+});
 
 
 
